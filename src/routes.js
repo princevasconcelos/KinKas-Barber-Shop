5a2429865aa6aed391e903db47ac487826a2b8a5
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-21 21:32:05 -0300

    create User is working

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 import express from 'express';
 
+import User from './app/models/User';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'prince',
+router.get('/', async (req, res) => {
+  console.log('chegou');
+  const user = await User.create({
+    name: 'prince',
+    email: 'praas@email.com',
+    password_hash: '2d2jd2j8d2j',
+  });
+
+  console.log(user);
+
+  return res.json({
+    message: 'ok',
   });
 });
 
